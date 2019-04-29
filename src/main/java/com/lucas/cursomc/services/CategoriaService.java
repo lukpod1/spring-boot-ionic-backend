26cda7d6884ad9a539c379c0249b515789ceb216
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2019-04-29 16:06:22 -0300

    validacao sintatica com bean validation

--- src/main/java/com/lucas/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucas/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lucas.cursomc.domain.Categoria;
+import com.lucas.cursomc.dto.CategoriaDTO;
 import com.lucas.cursomc.repositories.CategoriaRepository;
 import com.lucas.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria frontDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
